1b11d150699c9660cf1b7b4bb7f2cc5aa2cb3ffb
elabrtuu <elabrtuu@localhost>
2024-10-30 14:20:12 -0500

    studio 7

--- src/studio7/HockeyPlayer.java
+++ src/studio7/HockeyPlayer.java
@@ -1,9 +1,34 @@
 package studio7;
 
 public class HockeyPlayer {
-
+	private String name;
+	private int jersey;
+	private boolean hand;
+	private boolean left;
+	private boolean right;
+	
+	public HockeyPlayer() {
+		name = "";
+		jersey = 0;
+		hand = true;
+		left = true;
+		right = true;
+	}
+	
+	public HockeyPlayer(String name, int jersey, boolean hand, boolean left, boolean right) {
+		this.name = name;
+		this.jersey = jersey;
+		this.hand = hand;
+		this.left = left;
+		this.right = right;
+	}
+	
+	public int games() {
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HockeyPlayer h1 = new HockeyPlayer("John", 99, true, true, true);
 
 	}
 
